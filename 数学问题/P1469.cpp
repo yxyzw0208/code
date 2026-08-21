214e1c0de8f470539e31f6949be6c6985e86b263
yxyzw0208 <15554966919@163.com>
2026-08-21 14:51:12 +0800

    P1469 找筷子

---
+++ 数学问题/P1469.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    int ans = 0;
+    for(int i =0;i<n;++i){
+        int x;
+        cin>>x;
+        ans^=x;
+    }
+    cout<<ans;
+    return 0;
+}
